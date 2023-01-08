84f2bf740c33f836ec26f0f94e8672e855e3af27
Maida <maida.maxime@gmail.com>
2023-01-09 00:23:13 +0100

    Ajout handlesubmit et HTML pour le chat

--- client/script.js
+++ client/script.js
@@ -2,7 +2,7 @@ import bot from "./assets/bot.svg";
 import user from "./assets/user.svg";
 
 const form = document.querySelector("form");
-const chatContainer = document.querySelector("#chatContainer");
+const chatContainer = document.querySelector("#chat_container");
 
 let loadInterval;
 
@@ -33,7 +33,7 @@ const typeText = (element, text) => {
   }, 20);
 };
 
-// création d'une id unique
+// Fonction qui génère une id unique
 const generateUniqueId = () => {
   const timestamp = Date.now();
   const randomNumber = Math.random();
@@ -41,3 +41,44 @@ const generateUniqueId = () => {
 
   return `id-${timestamp}-${hexadecimanString}`;
 };
+
+const chatStripe = (isAi, value, uniqueId) => {
+  return `
+    <div class="wrapper ${isAi && "ai"}">
+      <div class="chat">
+        <div class="profile">
+          <img src="${isAi ? bot : user}"
+          alt="${isAi ? "bot" : "user"}"
+          />
+        </div>
+        <div class="message" id=${uniqueId}>${value}</div>
+      </div>
+    </div>
+    `;
+};
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  const data = new FormData(form);
+
+  // user's chatstripe
+  chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
+  form.reset();
+
+  // bot's chatstripe
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, "", uniqueId);
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueId);
+
+  loader(messageDiv);
+};
+
+form.addEventListener("submit", handleSubmit);
+form.addEventListener("keyup", (e) => {
+  if (e.keycode === 13) {
+    handleSubmit(e);
+  }
+});
